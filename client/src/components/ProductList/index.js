4aa105ff2c32a50f9554c1587702518f69c43d8d
Deni Mazzei <dconkell@gmail.com>
2022-01-06 22:07:47 -0600

    imported useDispatch and useSelector on productList index.js

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,15 +1,17 @@
-import React, { useEffect } from 'react';
-import ProductItem from '../ProductItem';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
-import { useQuery } from '@apollo/client';
-import { QUERY_PRODUCTS } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
-import spinner from '../../assets/spinner.gif';
+import React, { useEffect } from "react";
+import ProductItem from "../ProductItem";
+// import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { useQuery } from "@apollo/client";
+import { QUERY_PRODUCTS } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
+import spinner from "../../assets/spinner.gif";
+import { useDispatch, useSelector } from "react-redux";
 
 function ProductList() {
-  const [state, dispatch] = useStoreContext();
-
+  //const [state, dispatch] = useStoreContext();
+  const state = useSelector((s) => s);
+  const dispatch = useDispatch();
   const { currentCategory } = state;
 
   const { loading, data } = useQuery(QUERY_PRODUCTS);
@@ -21,10 +23,10 @@ function ProductList() {
         products: data.products,
       });
       data.products.forEach((product) => {
-        idbPromise('products', 'put', product);
+        idbPromise("products", "put", product);
       });
     } else if (!loading) {
-      idbPromise('products', 'get').then((products) => {
+      idbPromise("products", "get").then((products) => {
         dispatch({
           type: UPDATE_PRODUCTS,
           products: products,
