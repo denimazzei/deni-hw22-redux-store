bfe6ed940c3123491bee188769405e293910c814
Deni Mazzei <dconkell@gmail.com>
2022-01-09 21:25:15 -0600

    fixing issue with createStore in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,9 +14,7 @@ import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
-//import { StoreProvider } from "./utils/GlobalState";
-import { Provider } from "react-redux";
-import store from "./utils/store";
+import { StoreProvider } from "./utils/GlobalState";
 import Success from "./pages/Success";
 import OrderHistory from "./pages/OrderHistory";
 
@@ -44,8 +42,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          {/*<StoreProvider> */}
-          <Provider store={store}>
+          <StoreProvider>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -56,8 +53,7 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </Provider>
-          {/*</StoreProvider> */}
+          </StoreProvider>
         </div>
       </Router>
     </ApolloProvider>
